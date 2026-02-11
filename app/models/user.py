f3428e5ba7ac290f1e43184638e9805dd75935f2
Abduraufdev0788 <turkeynumber063@gmail.com>
2026-02-11 14:28:05 +0500

    models done

---
+++ app/models/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy import String, Column, BigInteger, DateTime
+
+from app.models.base import Base
+
+class User(Base):
+
+    __tablename__ = 'users'
+
+
+    id = Column(BigInteger, primary_key=True)
+    telegram_id = Column(
+        BigInteger,
+        unique=True,
+        nullable=False,
+        index=True,
+    )
+    phone_number = Column(String(20))
+    fullname = Column(String(150), nullable= False)
+
+    orders = relationship( "Order", back_populates="user")
